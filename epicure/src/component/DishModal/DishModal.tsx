75a7bfc1a88c78884d5571bf7b4962e03438d854
eran <eranavru100@gmail.com>
2023-02-22 10:15:41 +0200

    modal - without functionality

--- epicure/src/component/DishModal/DishModal.tsx
+++ epicure/src/component/DishModal/DishModal.tsx
@@ -15,7 +15,7 @@ const DishModal: React.FC<IModal> = (props: IModal) => {
   if(props.open === false) return null
   return (
     <>
-    <div id="modal-content">
+    <div id="modal-content" onClick={props.onClose}>
       <div id="modal">
         <button id="close" onClick={props.onClose} >X</button>
         <Card
@@ -26,24 +26,24 @@ const DishModal: React.FC<IModal> = (props: IModal) => {
           price = {clickedDish?.price}
         />
         <div id="modal-bottom" >
-            <form action="">
+            <form>
             <h1>Choose a side</h1>
               {clickedDish?.sides.map((side) => 
-              <label htmlFor=""><input value={side} type="radio" />{side}</label>
+              <label htmlFor=""><input name="sides" value={side} type="radio" />{side}</label>
               )}
             </form>
-            <form action="">
+            <form>
             <h1>Changes</h1>
             {clickedDish?.changes.map((change) => 
-              <label htmlFor=""><input value={change} type="checkbox" />{change}</label>
+              <label htmlFor=""><input  value={change} type="checkbox" />{change}</label>
               )}
             </form>
           <div id="quantity">
             <h1>Quantity</h1>
             <div>
-            <button>-</button>
+            <button><img src="/image/icons/minus.svg" alt="minus" /></button>
             <p>2</p>
-            <button>+</button>
+            <button><img src="/image/icons/plus.svg" alt="plus"  /></button>
             </div>
           </div>
           <button id="cart-btn">ADD TO CART</button>
